a13fe4b5624e69d7e3ac7d9e3e944546bef775de
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-07-29 16:27:21 -0300

    create Class,Teacher

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import {app} from "./app"
+import { addClass } from "./endpoints/addClass";
 import { addStudent } from "./endpoints/addStudent"
+import { addTeachers } from "./endpoints/addTeachers";
+
 
 app.post("/createStudent", addStudent);
+
+
+app.post("/createTeacher", addTeachers);
+
+app.post("/createClass", addClass);
